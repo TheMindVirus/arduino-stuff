47d41b48f457166ca71cccc2b83870f81f032513
Alastair Cota <a1.cota.365@gmail.com>
2023-02-16 14:27:02 +0000

    Add files via upload

---
+++ UNO_ST7735/avrdis.bat
@@ -0,0 +1,6 @@
+@echo off
+set SKETCH=UNO_ST7735
+set PATH=%PATH%;%PROGRAMFILES(X86)%/Arduino/hardware/tools/avr/bin/
+avr-objcopy -I ihex -O elf32-avr %SKETCH%.ino.standard.hex %SKETCH%.elf
+avr-objdump -D %SKETCH%.elf > %SKETCH%.txt
+rem pause
